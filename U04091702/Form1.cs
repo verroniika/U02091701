e1bd1c8cc9bb0b2f249c3efb7c4a5b33c43be213
Veronika <veronika@ranta.com>
2017-09-05 13:06:46 +0200

    Första

--- U04091702/Form1.cs
+++ U04091702/Form1.cs
@@ -15,6 +15,62 @@ namespace U04091702
         public Form1()
         {
             InitializeComponent();
+            button1.Text = "+";
+            button2.Text = "-";
+            button3.Text = "x";
+            button4.Text = "/";
+            label1.Text = "Tal 1   -->";
+            label2.Text = "Tal 2   -->";
+            label3.Text = "Summan av talen är: ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i;
+            i = Int32.Parse(textBox1.Text);
+
+            int j;
+            j = Int32.Parse(textBox2.Text);
+
+            listBox1.Items.Add(i + j);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i;
+            i = Int32.Parse(textBox1.Text);
+
+            int j;
+            j = Int32.Parse(textBox2.Text);
+
+            listBox1.Items.Add(i - j);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i;
+            i = Int32.Parse(textBox1.Text);
+
+            int j;
+            j = Int32.Parse(textBox2.Text);
+
+            listBox1.Items.Add(i * j);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            float i;
+            i = float.Parse(textBox1.Text);
+
+            float j;
+            j = float.Parse(textBox2.Text);
+
+            listBox1.Items.Add(i / j);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
